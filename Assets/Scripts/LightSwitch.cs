491704431427715685129fe7d0ba11f5a5e97062
cruzifixu <franchezcacruz09@gmail.com>
2023-01-10 22:07:14 +0100

    light switch

--- Assets/Scripts/LightSwitch.cs
+++ Assets/Scripts/LightSwitch.cs
@@ -6,11 +6,13 @@ public class LightSwitch : MonoBehaviour
 {
     public GameObject mylight1;
     public GameObject mylight2;
+    public GameObject sWitch;
     // Start is called before the first frame update
     void Start()
     {
         mylight1 = GameObject.FindGameObjectWithTag("light");
         mylight2 = GameObject.FindGameObjectWithTag("light");
+        sWitch = GameObject.FindGameObjectWithTag("sWitch");
     }
 
     // Update is called once per frame
@@ -25,12 +27,14 @@ public class LightSwitch : MonoBehaviour
         {
             mylight1.SetActive(false);
             mylight2.SetActive(false);
+            sWitch.SetActive(true);
             Debug.Log("OFF");
         }
         else
         {
             mylight1.SetActive(true);
             mylight2.SetActive(true);
+            sWitch.SetActive(false);
             Debug.Log("ON");
         }
     }
